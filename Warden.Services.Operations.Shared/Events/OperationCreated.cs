3d7c8934a14d64395dd11926d0b005db7538b339
Piotr Gankiewicz <spetz90@gmail.com>
2016-12-17 08:26:29 +0100

    Shared project

---
+++ Warden.Services.Operations.Shared/Events/OperationCreated.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Warden.Services.Operations.Shared.Events
+{
+    public class OperationCreated : IAuthenticatedEvent
+    {
+        public Guid RequestId { get; }
+        public string Name { get; }
+        public string UserId { get; }
+        public string Origin { get; }
+        public string Resource { get; }
+        public string State { get; }
+        public DateTime CreatedAt { get; }
+
+        protected OperationCreated()
+        {
+        }
+
+        public OperationCreated(Guid requestId, string name,
+            string userId, string origin, string resource,
+            string state, DateTime createdAt)
+        {
+            RequestId = requestId;
+            Name = name;
+            UserId = userId;
+            Origin = origin;
+            Resource = resource;
+            State = state;
+            CreatedAt = createdAt;
+        }
+    }
+}
